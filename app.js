487478747851ea0e51e1ad27a173e14b557b5d2a
Pavel Emelianov <emeliarulit@gmail.com>
2023-03-23 20:51:17 +0300

    burger menu withous submenu

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ const menuSlide = () => {
   //   const nav = document.querySelector('.menu-item');
 
   burger.addEventListener('click', () => {
-    document.querySelector('.header').classList.toggle('open');
+    document.querySelector('.nav-menu').classList.toggle('open');
     // nav.classList.toggle('menu-active');
-    burger.classList.toggle('toggle');
+    burger.classList.toggle('active');
+    burger.classList.add('white-cross');
+  });
+};
+
+const showItem = () => {
+  const itemMenu = document.querySelectorAll('.menu-item');
+  const drop = document.querySelector('.dropdown');
+
+  //   const hide = document.querySelectorAll('.hide')
+
+  itemMenu.forEach((item) => {
+    item.addEventListener('click', () => {
+      console.log(item);
+      drop.classList.toggle('is-open');
+    });
   });
 };
 
 menuSlide();
+
+showItem();
+
+// showItem();

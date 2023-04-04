8c3ca2afd2af7e246cadf09b26e419bcf8522ed2
Pavel Emelianov <emeliarulit@gmail.com>
2023-04-04 18:02:25 +0300

    Display items in header from an array of objects
    Items shown in the header (Why Spring, Learn, etc.) and the
    same for the mobile version where these items are shown in
    the burger menu

--- header.js
+++ header.js
@@ -11,8 +11,13 @@ let data = [
       'Serverless',
       'Batch',
     ],
+    id: 1,
   },
-  { title: 'Learn', items: ['Overview', 'Quickstart', 'Guides', 'Blog'] },
+  {
+    title: 'Learn',
+    items: ['Overview', 'Quickstart', 'Guides', 'Blog'],
+    id: 2,
+  },
   {
     title: 'Projects',
     items: [
@@ -29,11 +34,31 @@ let data = [
       'Spring Tools 4',
       'Spring Initializr',
     ],
+    id: 3,
   },
-  { title: 'Academy', items: ['Courses', 'Get Certified'] },
-  { title: 'Support', items: ['Overview', 'Security Advisories'] },
-  { title: 'Community', items: ['Overview', 'Events', 'Team', 'Govno'] },
+  { title: 'Academy', items: ['Courses', 'Get Certified'], id: 4 },
+  { title: 'Support', items: ['Overview', 'Security Advisories'], id: 5 },
+  { title: 'Community', items: ['Overview', 'Events', 'Team'], id: 6 },
 ];
+
+const menuSlide = () => {
+  const burger = document.querySelector('.burger');
+
+  burger.addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('open');
+    burger.classList.toggle('active');
+    burger.classList.add('white-cross');
+    if (burger.classList.contains('active')) {
+      document.body.style.overflow = 'hidden';
+      document.querySelector('.search-wrapper').classList.add('h');
+    } else {
+      document.body.style.overflow = 'auto';
+      document.querySelector('.search-wrapper').classList.remove('h');
+    }
+  });
+};
+
+menuSlide();
 
 function createNavMenu() {
   const userDataTemplate = document.querySelector('[data-header-template]');
@@ -42,10 +67,10 @@ function createNavMenu() {
   for (let i = 0; i < data.length; i++) {
     const grid = userDataTemplate.content.cloneNode(true).children[0];
     const header = grid.querySelector('[data-header]');
-    // const body = grid.querySelector('[data-body]');
     const dropDown = grid.querySelectorAll('[data-drop]');
     const icon = grid.querySelector('[data-icon]');
     header.textContent = data[i].title;
+    header.setAttribute('data-id', data[i].id);
     for (let k = 0; k < dropDown.length; k++) {
       data[i].items.map((el) => {
         dropDown[k].innerHTML += `<li 
@@ -59,4 +84,56 @@ function createNavMenu() {
   }
 }
 
+function createBurgerIsBlock() {
+  let flag = false;
+  document.querySelector('.burger').addEventListener('click', () => {
+    if (!flag) {
+      const allMenuLink = document.querySelectorAll('.menu-link');
+      for (let i = 0; i < allMenuLink.length; i++) {
+        const arrow = document.createElement('span');
+        arrow.classList.add('arrow');
+        allMenuLink[i].append(arrow);
+      }
+
+      allMenuLink.forEach((item) => {
+        item.addEventListener('click', (e) => {
+          e.target.children[1].classList.toggle('arrow-reverse');
+        });
+      });
+
+      const allMenuItem = document.querySelectorAll('.menu-item');
+
+      for (let i = 0; i < allMenuItem.length; i++) {
+        const isBlock = document.createElement('div');
+
+        isBlock.classList.add('is-block');
+        isBlock.classList.add('hide');
+        isBlock.setAttribute('data-b', data[i].id);
+
+        allMenuItem[i].after(isBlock);
+
+        let isReal = false;
+        allMenuItem.forEach((item) => {
+          item.addEventListener('click', (e) => {
+            let currTarget = e.target.dataset.id;
+
+            if (currTarget == isBlock.getAttribute('data-b')) {
+              isBlock.classList.toggle('hide');
+              if (!isReal) {
+                data[i].items.map((el) => {
+                  isBlock.innerHTML += `<a class="block-link">${el}</a>`;
+                });
+              }
+              isReal = true;
+            }
+          });
+        });
+      }
+      flag = true;
+    }
+  });
+}
+
 createNavMenu();
+
+createBurgerIsBlock();
